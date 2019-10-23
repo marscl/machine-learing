e1b41766131e753e53a5d43915ec0879d84a5dcb
marscl <flytomars01@gmail.com>
2019-10-23 14:36:27 +0800

    gg

--- tree.py
+++ tree.py
@@ -1,5 +1,7 @@
 # 决策树
 from math import log
+import operator
+import treePlotter
 
 
 def create_dataset():
@@ -56,7 +58,7 @@ def choose_best_feature(dataset):
             subDataset = split_dataset(dataset, i, val)
             prob = len(subDataset) / float(len(dataset))
             newEntropy += prob * calc_shannonent(subDataset)
-        print("第{}个特征香农熵为：{}".format(i,newEntropy))
+        print("第{}个特征香农熵为：{}".format(i, newEntropy))
         infoGain = baseEntropy - newEntropy
         if infoGain > bestInfoGain:
             bestFeature = i
@@ -64,14 +66,75 @@ def choose_best_feature(dataset):
     return bestFeature
 
 
-dataset, labels = create_dataset()
-print(dataset)
-print(calc_shannonent(dataset))
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedCount = sorted(classCount.items(), operator.itemgetter(1), True)
+    return sortedCount[0][0]
 
-# dataset[0][-1] = 'maybe'
-# print(dataset)
-# print(calc_shannonent(dataset))
 
-# print(split_dataset(dataset, 0, 1))
-# print(split_dataset(dataset, 0, 0))
-print(choose_best_feature(dataset))
+def createTree(dataset, labels):
+    # 类别相同,停止递归
+    classlist = [x[-1] for x in dataset]
+    if classlist.count(classlist[0]) == len(classlist):
+        return classlist[0]
+    # 划分完所有特征
+    if len(dataset[0]) == 1:
+        return majorityCnt(classlist)
+
+    best_feat = choose_best_feature(dataset)
+    best_feat_label = labels[best_feat]
+    my_tree = {best_feat_label: {}}
+    del (labels[best_feat])
+    feat_val = [x[best_feat] for x in dataset]
+    unique_val = set(feat_val)
+    for val in unique_val:
+        sublabels = labels[:]
+        my_tree[best_feat_label][val] = createTree(split_dataset(dataset, best_feat, val), sublabels)
+
+    return my_tree
+
+
+def classify(tree, featlabels, testvec):
+    firstLable = list(tree.keys())[0]
+    seconddic = tree[firstLable]
+    featIndex = featlabels.index(firstLable)
+    for key in seconddic.keys():
+        if key == testvec[featIndex]:
+            if type(seconddic[key]).__name__ == 'dict':
+                classLabel = classify(seconddic[key], featlabels, testvec)
+            else:
+                classLabel = seconddic[key]
+
+    return classLabel
+
+
+if __name__ == '__main__':
+    dataset, labels = create_dataset()
+    print(dataset)
+    #print(calc_shannonent(dataset))
+
+    # dataset[0][-1] = 'maybe'
+    # print(dataset)
+    # print(calc_shannonent(dataset))
+
+    # print(split_dataset(dataset, 0, 1))
+    # print(split_dataset(dataset, 0, 0))
+    #print(choose_best_feature(dataset))
+
+    tree = createTree(dataset, labels.copy())
+    print(tree)
+
+    # treePlotter.createPlot(tree)
+    print(classify(tree, labels, [1, 1]))
+    print(classify(tree, labels, [0, 1]))
+
+    # 隐性眼镜
+    fr = open('datas/lenses.txt')
+    lenses = [x.strip().split('\t') for x in fr.readlines()]
+    labels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    lensestree=createTree(lenses,labels)
+    print(lensestree)
+    treePlotter.createPlot(lensestree)
